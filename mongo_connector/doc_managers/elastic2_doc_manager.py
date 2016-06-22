6d26c559a9721a1504e37a5929744e11d3407994
Anthony Atkinson <aatkinson@powerchordsystem.com>
2016-06-22 08:26:04 -0400

    Addition of AWS signing authentication.

--- mongo_connector/doc_managers/elastic2_doc_manager.py
+++ mongo_connector/doc_managers/elastic2_doc_manager.py
@@ -20,12 +20,13 @@ Elasticsearch.
 import base64
 import logging
 import warnings
+import requests
 
 from threading import Timer
 
 import bson.json_util
 
-from elasticsearch import Elasticsearch, exceptions as es_exceptions
+from elasticsearch import Elasticsearch, exceptions as es_exceptions, connection as es_connection
 from elasticsearch.helpers import bulk, scan, streaming_bulk
 
 from mongo_connector import errors
@@ -35,6 +36,8 @@ from mongo_connector.constants import (DEFAULT_COMMIT_INTERVAL,
 from mongo_connector.util import exception_wrapper, retry_until_ok
 from mongo_connector.doc_managers.doc_manager_base import DocManagerBase
 from mongo_connector.doc_managers.formatters import DefaultDocumentFormatter
+
+from requests_aws4auth import AWS4Auth
 
 wrap_exceptions = exception_wrapper({
     es_exceptions.ConnectionError: errors.ConnectionFailed,
@@ -56,8 +59,16 @@ class DocManager(DocManagerBase):
                  unique_key='_id', chunk_size=DEFAULT_MAX_BULK,
                  meta_index_name="mongodb_meta", meta_type="mongodb_meta",
                  attachment_field="content", **kwargs):
+        aws = **kwargs.get('aws', {'access_id': '', 'secret_key': '', 'region': ''})
+        client_options = **kwargs.get('clientOptions', {})
+        if 'aws' in kwargs:
+            aws_auth = AWS4Auth(aws['access_id'], aws['secret_key'], aws['region'], 'es')
+            client_options['http_auth'] = aws_auth
+            client_options['use_ssl'] = True
+            client_options['verify_certs'] = True
+            client_options['connection_class'] = es_connection.RequestsHttpConnection
         self.elastic = Elasticsearch(
-            hosts=[url], **kwargs.get('clientOptions', {}))
+            hosts=[url], client_options)
         self.auto_commit_interval = auto_commit_interval
         self.meta_index_name = meta_index_name
         self.meta_type = meta_type
